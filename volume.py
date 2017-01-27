32bcbb6a3c5545c70efd50d5ca78f180f114d029
alunno <alunno@lab03-01>
2017-01-27 09:57:24 +0100

    Upgrade volume

--- volume.py
+++ volume.py
@@ -1,10 +1,11 @@
 import math
-figura=input("Effettua una scelta: (1 - cubo, 2 - sfera) ")
+print "Calcolo del volume di un cubo o di una sfera"
+figura=input("Effettua una scelta: (1 - cubo, 2 - sfera)? ")
 if figura == 1:
     lato=input("lato del cubo: ")
-    vol1=lato**3
-    print "Il cubo di lato ",lato," ha volume ",vol1
+    vol=lato**3
+    print "Il cubo di lato ",lato," ha volume ",vol
 else:
     raggio=input("raggio della sfera: ")
-    vol2=(4.0/3.0) * math.pi * (raggio**3)
-    print "La sfera di raggio ",raggio," ha volume ",vol2
+    vol=(4.0/3.0) * math.pi * (raggio**3)
+    print "La sfera di raggio ",raggio," ha volume ",vol
