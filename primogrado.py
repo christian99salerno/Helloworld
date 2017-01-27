25da1c2e222d87b0c37e5b402b694512d44e1cea
alunno <alunno@lab03-01>
2017-01-27 09:50:03 +0100

    Aggiunto il file dell'equazione di primo grado in python

---
+++ primogrado.py
@@ -0,0 +1,15 @@
+a=input("Inserisci a ")
+b=input("Inserisci b ")
+if a==0 and b!=0:
+    print "L'equazione e' impossibile"
+elif a==0 and b==0:
+    print "L'equazione e' indeterminata"
+else:
+    x=-b/a
+    print "La x vale ",x
+
+
+
+
+    
+
