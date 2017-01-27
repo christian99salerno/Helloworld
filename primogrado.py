e95b01c7b6e8244795d00720abc6c2e508fa535f
christian99salerno <christian.salerno05@gmail.com>
2017-01-27 14:48:46 +0100

    Rinnovo

--- primogrado.py
+++ primogrado.py
@@ -1,9 +1,9 @@
 a=input("Inserisci a ")
 b=input("Inserisci b ")
-if a==0 and b!=0:
-    print "L'equazione e' impossibile"
-elif a==0 and b==0:
+if a==0 and b==0:
     print "L'equazione e' indeterminata"
+elif a==0:
+    print "L'equazione e' impossibile"
 else:
     x=-b/a
     print "La x vale ",x
