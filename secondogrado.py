08ed3e4e3b16f26b63a3b2caf996fc3319b2a202
christian99salerno <christian.salerno05@gmail.com>
2017-01-27 20:56:15 +0100

    step by step

--- secondogrado.py
+++ secondogrado.py
@@ -1,7 +1,16 @@
 import math
 
-a=input("Inserisci il valore di a ")
-b=input("Inserisci il valore di b ")
-c=input("Inserisci il valore di c ")
+a=input("Inserisci a ")#coefficiente del termine con la x^2
+b=input("Inserisci b ")#coefficiente del termine con la x
+c=input("Inserisci c ")#termine noto
 
+# ax^2+bx+c=0
 
+if c==0:
+#equazione incompleta pura ax^2+bx=0
+    
+elif b==0:    
+#equazione incompleta spuria ax^2+c=0
+
+else:
+#equazione completa ax^2+bx+c=0
