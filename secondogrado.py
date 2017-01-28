7bf083ee16386846149687402d384dead9334814
christian99salerno <christian.salerno05@gmail.com>
2017-01-28 16:43:00 +0100

    secondogrado terminato

--- secondogrado.py
+++ secondogrado.py
@@ -5,12 +5,18 @@ b=input("Inserisci b ")#coefficiente del termine con la x
 c=input("Inserisci c ")#termine noto
 
 # ax^2+bx+c=0
+delta=b**2-4*a*c
 
-if c==0:
-#equazione incompleta pura ax^2+bx=0
-    
-elif b==0:    
-#equazione incompleta spuria ax^2+c=0
 
-else:
-#equazione completa ax^2+bx+c=0
+if delta>0:
+  x1=(-b+math.sqrt(delta))/2*a
+  x2=(-b-math.sqrt(delta))/2*a
+  print "L'equazione ha 2 soluzioni:",x1,"e",x2
+
+elif delta==0:
+    x=-b/2*a
+    print "L'equazione ha 1 soluzione:",x
+
+else:    
+    print "L'equazione non ammette soluzioni"
+
