2cd151c891d11169ddeca1f88f373a8256a0d3b7
alunno <alunno@lab03-01>
2017-01-30 12:02:49 +0100

    programma completato con successo

--- secondogrado.py
+++ secondogrado.py
@@ -4,19 +4,20 @@ a=input("Inserisci a ")#coefficiente del termine con la x^2
 b=input("Inserisci b ")#coefficiente del termine con la x
 c=input("Inserisci c ")#termine noto
 
-# ax^2+bx+c=0
-delta=b**2-4*a*c
+if a==0 and b==0 and c==0:
+    print "L'equazione e' indeterminata"
+elif a==0 and b==0:
+    print "L'equazione e' impossibile"
+elif a==0:
+    x=-c/b
+    print "La x vale",x
+else:
+    delta = (b**2) - (4.0*a*c)
+    if delta>=0:
+        x1 = ((-b) + math.sqrt(delta)) / (2.0*a)
+        x2 = ((-b) - math.sqrt(delta)) / (2.0*a)
+        print "I risultati dell'equazione sono",x1, "e",x2
+    else:
+        print "L'equazione non ha soluzioni"
 
-
-if delta>0:
-  x1=(-b+math.sqrt(delta))/2*a
-  x2=(-b-math.sqrt(delta))/2*a
-  print "L'equazione ha 2 soluzioni:",x1,"e",x2
-
-elif delta==0:
-    x=-b/2*a
-    print "L'equazione ha 1 soluzione:",x
-
-else:    
-    print "L'equazione non ammette soluzioni"
 
